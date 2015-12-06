0d179287efe63c7a9c37f0c853f2b0154daff76c
Christopher Butcher <cbutcher@gmail.com>
2015-12-06 15:48:00 -0500

    mailgun setup

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :port           => 587,
-  :address        => 'smtp.mailgun.org',
-  :user_name      => 'postmaster@app19873111.mailgun.org',
-  :password       => 'bd5942ecacc6ecda5b9af681097b201b',
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'butcher-xmas.heroku.com',
   :authentication => :plain,
 }
